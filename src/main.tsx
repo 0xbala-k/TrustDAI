371e9c6b7a3ae4066b0bb880b1d683c6a74e51d0
0xmuralik <krishna192reddy@gmail.com>
2025-02-28 21:28:54 -0700

    add okto provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,19 @@
+import { OktoProvider } from "@okto_web3/react-sdk";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+const config = {
+    environment: "sandbox",
+    clientPrivateKey: import.meta.env.VITE_CLIENT_PRIV_KEY,
+    clientSWA: import.meta.env.VITE_CLIENT_SWA,
+};
+
+createRoot(document.getElementById("root")!).render(
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+        <OktoProvider config={config}>
+            <App />
+        </OktoProvider>
+    </GoogleOAuthProvider>
+);
