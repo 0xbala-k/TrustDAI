fd9294043f94885ba38376e5386ca538b92f5bfd
0xmuralik <krishna192reddy@gmail.com>
2025-02-28 23:32:15 -0700

    okto dashboard page

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "next-themes"; // Add this
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { Dashboard } from "./pages/OktoDashboard";
 
-const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <ThemeProvider attribute="class" defaultTheme="dark"> {/* Set dark as default */}
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </ThemeProvider>
-  </QueryClientProvider>
-);
+function App (){
+  return(
+      <ThemeProvider attribute="class" defaultTheme="dark"> {/* Set dark as default */}
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          
+              <BrowserRouter>
+                <Routes>
+                  <Route path="/" element={<Index />} />
+                  <Route path="/okto" element={<Dashboard/>}/>                    
+                  <Route path="*" element={<NotFound />} />
+                </Routes>
+              </BrowserRouter>
+            
+        </TooltipProvider>
+      </ThemeProvider>
+  );
+}
 
 export default App;
